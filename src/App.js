f02960d7330d1636b308bf0bf12c74c1d39df686
chadd williams <chadd980@hotmail.com>
2017-02-10 03:59:41 -0500

    added style and most functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,209 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.min.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      minutes: 25,
+      breakMinutes: 5,
+      seconds: 0,
+      count: 0,
+      on: false,
+      end: false
+    }
+  }
+
+  componentDidUpdate() {
+    if(this.state.minutes === 0 && this.state.seconds === 0) {
+
+    }
+  }
+
+  handleDecrementBreak() {
+    this.setState({
+      breakMinutes: this.state.breakMinutes - 1
+    })
+  }
+
+  handleIncrementBreak() {
+    this.setState({
+      breakMinutes: this.state.breakMinutes + 1
+    })
+  }
+
+  handleDecrement() {
+    if(this.state.on === false) {
+      this.setState({
+        minutes: this.state.minutes - 1
+      })
+    }
+  }
+
+  handleIncrement() {
+    if(this.state.on === false) {
+      this.setState({
+        minutes: this.state.minutes + 1
+      })
+    }
+  }
+
+  reduceMinutes() {
+    this.setState({
+      minutes: this.state.minutes - 1,
+      seconds: 59
+    })
+  }
+
+  reduceSeconds() {
+    this.setState({
+      seconds: this.state.seconds - 1
+    })
+  }
+
+  endCount() {
+    end: !this.state.end
+  }
+
+  toggleCount() {
+    this.setState({
+      on: !this.state.on
+    })
+  }
+
+  startCount() {
+    this.setState({
+      on: !this.state.on,
+      count: this.state.count + 1
+    })
+  }
+
+  restartCount() {
+    this.setState({
+      on: !this.state.on
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <Header />
+        <Clock restartCount={this.restartCount.bind(this)} startCount={this.startCount.bind(this)} toggleCount={this.toggleCount.bind(this)} endCount={this.endCount.bind(this)} minutes={this.state.minutes} seconds={this.state.seconds} count={this.state.count} on={this.state.on} end={this.state.end} reduceMinutes={this.reduceMinutes.bind(this)} reduceSeconds={this.reduceSeconds.bind(this)}/>
+        <Time on={this.state.on} increment={this.handleIncrement.bind(this)} decrement={this.handleDecrement.bind(this)} minutes={this.state.minutes} />
+        <Break increment={this.handleIncrementBreak.bind(this)} decrement={this.handleDecrementBreak.bind(this)} breakMinutes={this.state.breakMinutes} />
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <p>Pomodoro Clock</p>
+      </div>
+    )
+  }
+}
+
+class Clock extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      minutes: 25,
+      seconds: 0,
+      count: 0,
+      on: false,
+      end: false
+    }
+  }
+
+  countdown() {
+    var self = this
+    let timeInterval = setInterval(function() {
+      if(self.props.seconds === 0) {
+        self.props.reduceMinutes()
+      } else {
+        self.props.reduceSeconds()
+      }
+      if (self.props.minutes === 0 && self.props.seconds === 0) {
+        this.props.endCount()
+      }
+      if(self.props.end === true || self.props.on === false) {
+        clearInterval(timeInterval)
+      }
+    },1000);
+  }
+
+  handleClick() {
+    if(this.props.on){
+      this.props.toggleCount()
+    } else if (this.props.count === 0) {
+      this.props.startCount()
+      this.countdown()
+    } else {
+      this.props.restartCount()
+      this.countdown()
+    }
+  }
+
+  render() {
+    return (
+      <div className="clock" style={{cursor:'pointer'}} onClick={() => this.handleClick()}>
+        <div className="ses">Session</div>
+        {("0" + this.props.minutes).slice(-2)}:{("0" + this.props.seconds).slice(-2)}
+      </div>
+    );
+  }
+}
+
+
+class Time extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      minutes: this.props.minutes
+    }
+  }
+
+  timeDecrement() {
+    this.props.decrement()
+    this.setState({
+      minutes: this.state.minutes - 1
+    })
+  }
+
+  timeIncrement() {
+    this.props.increment()
+    this.setState({
+      minutes: this.state.minutes + 1
+    })
+  }
+
+  render() {
+    return (
+      <div className="small-box ses-length">
+        <p>Session Length</p>
+        <div className="length">
+          <div className="changer decrement" style={{cursor:'pointer'}} onClick={() => this.timeDecrement()}>-</div>
+          <p>{this.state.minutes}</p>
+          <div className="changer increment" style={{cursor:'pointer'}} onClick={() => this.timeIncrement()}>+</div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      </div>
+    );
+  }
+}
+
+class Break extends Component {
+  render() {
+    return (
+      <div className="small-box">
+        <p>Break Length</p>
+        <div className="length">
+          <div className="changer decrement" style={{cursor:'pointer'}} onClick={() => this.props.decrement()}>-</div>
+          <p>{this.props.breakMinutes}</p>
+          <div className="changer decrement" style={{cursor:'pointer'}} onClick={() => this.props.increment()}>+</div>
+        </div>
       </div>
     );
   }
